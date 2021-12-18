caf03211d7c07e903b60150894c90ded860464fa
yuyuyuriko78 <yuyuyuriko78@gmail.com>
2021-12-18 15:11:27 +0900

    パズル認証処理を修正

--- README.md
+++ README.md
@@ -3,12 +3,45 @@ Capy Puzzle CAPTCHA サンプルプログラム
 
 ## 事前準備
 
-Composer install を行う
+### 必要なライブラリのインストール
+`composer.json`があるディレクトリにて以下コマンドを実行し、必要なライブラリをインストールします。 
 
 ```shell
-$ compose insatall
+$ composer insatall
+```
+上記コマンドが完了すると、`composer.lock`ファイルと、`vendor`ディレクトリが設置されます。
+
+### 環境変数を使用するための準備
+環境変数を`.env`ファイルにて定義し、プログラム内で使用します。
+
+`.env`ファイルを使用するためのライブラリを以下コマンドで導入します。
+
+```bash
+$ composer require vlucas/phpdotenv
 ```
 
-※ `vendor` ディレクトリを設置
+`.gitignore`ファイルに、`.env`ファイルを追記します。
+今回の`.gitignore`の中身は以下のようになっています。
 
-`puzzle_sample.php` を HTTP から送信
+```text:.gitignore
+/vendor/
+.idea/
+.env
+```
+
+`.env`ファイルを、プロジェクトのルートに作成します。　
+
+```bash
+$ touch .env
+```
+
+`.env`ファイルに以下の環境変数を定義します。
+hogeには御社のパズルID、
+fugaには御社のプライベートキーを入れてください。
+
+```text:.env
+PUZZLE_KEY="PUZZLE_hoge"
+PRIVATE_KEY="KEY_fuga"
+```
+
+`puzzle_sample.php` をHTTPからPOSTで送信すると、パズルCAPTCHAが埋め込まれたログインページが表示されます。
--- puzzle_sample.php
+++ puzzle_sample.php
@@ -2,23 +2,38 @@
 header('Content-Type: text/html; charset=UTF-8');
 require_once __DIR__ . '/vendor/autoload.php';
 
-// HTTP リクエストを送るためのクライアントを設定
+// .envファイルを使用する
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
+// HTTP リクエストを送るためのクライアントを設定
 use GuzzleHttp\Client;
 
+// Capyの死活監視結果取得
+function check_capy_status(){
+    $is_available = false;
+    $fh = fopen("https://sip-operation.capy.me/status/puzzle", "r");
+    if ($fh) {
+      $line = fgets($fh, 1024);
+      $is_available = ($line == "operational");
+      fclose($fh);
+    }
+    return $is_available;
+}
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $private_key = "KEY_XXXXXXXXXXXXXX";
+    $puzzle_key = $_ENV['PUZZLE_KEY'];      // .envファイルからパズルキーを取得
+    $private_key = $_ENV['PRIVATE_KEY'];    // .envファイルからプライベートキーを取得
     $uri = "https://jp.api.capy.me/";
     $client = new Client(["base_uri" => $uri, "timeout" => 5000, "stream" => false]);
     try {
-        $challenge_key = $_POST['capy_challengekey'];  // POSTパラメータから Challenge Key を設定
-        $answer = $_POST['capy_answer']; // POSTパラメータから回答データを取得
+        $challenge_key = $_POST['capy_challengekey'];        // POSTパラメータから Challenge Key を設定
+        $answer = $_POST['capy_answer'];                     // POSTパラメータから回答データを取得
         $param_puzzle = [
             "form_params" => [
-                "capy_privatekey" => $private_key, // Capy Console より PRIVATE KEY を設定
-                "capy_challengekey" => $challenge_key, // Challenge Key を設定
-                "capy_answer" => $answer // 回答データを送信
+                "capy_privatekey" => $private_key,          // プライベートキーを設定
+                "capy_challengekey" => $challenge_key,      // Challenge Keyを設定
+                "capy_answer" => $answer                    // 回答データを設定
             ]];
         $url_puzzle = 'puzzle/verify';
         $response_puzzle = $client->post($url_puzzle, $param_puzzle);
@@ -37,19 +52,19 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 </head>
 <body>
 
-<?php if (!empty($message)): ?>
-    <p><?= $message ?></p>
-<?php endif; ?>
-
-<div id='capy-wrapper'>
+<div id="capy-wrapper">
     <form method="post">
         <input type="text" name="userid" placeholder="ID"><br/>
         <input type="password" name="password" placeholder="パスワード">
         <input type="submit" value="ログイン">
-        <?php # JavaScript からパズルCAPTCHAを呼び出し ?>
-        <script type="text/javascript"
-                src="https://jp.api.capy.me/puzzle/get_js/?k=PUZZLE_XXXXXXXXXXXXX">
-        </script>
+        <div id="capy-captcha">
+            <?php 
+                if (check_capy_status()) {  
+                    $puzzle_key_escape = htmlspecialchars($puzzle_key, ENT_QUOTES, 'UTF-8');
+                    echo "<script type='text/javascript' src='https://jp.api.capy.me/puzzle/get_js/?k={$puzzle_key_escape}'></script>";                    
+                }
+            ?>
+        </div>
     </form>
 </body>
 </html>
